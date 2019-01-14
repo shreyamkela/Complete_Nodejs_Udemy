c9ddb7c1e846213bb8dbb4e978be26aa7aef77c2
shrke <shreyamkela@gmail.com>
2019-01-14 01:12:01 -0800

    added section4_weather-app and async-basics.js in it

--- section4_weather-app/playground/async-basics.js
+++ section4_weather-app/playground/async-basics.js
@@ -1,7 +1,7 @@
 console.log('Starting app');
 
 // setTimeout will get back and print after 2 sec irrespective of what the program is doing at that very moment. Thus the program can continue and setTimeout returns asynchronously back after 2s, thus creating async call.
-setTimeout(() => { // Callback func
+setTimeout(() => { // Callback func - Call back funcs are funcs that have another func as their argument. When the setTimeout() was defined in node libraries, it must have been defined in a similar way as this - var setTimeout = (function, delay) => {...}. This is the syntax for defining callback funcs
     console.log('Inside of Callback1a');
     console.log('Inside of Callback1b');
 }, 2000); // delay
@@ -12,6 +12,6 @@ setTimeout(() => {
 
 setTimeout(() => {
     console.log('Inside of Callback3');
-}, 0);
+}, 0); // This is printed after 'Finishing up' as when the main() finishes then only all the items from callback queue are executed
 
 console.log('Finishing up');
