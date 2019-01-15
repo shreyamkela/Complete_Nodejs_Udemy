aa529d94df2359f2ae0057d13fc37e1177a0569f
shrke <shreyamkela@gmail.com>
2019-01-14 23:23:23 -0800

    pretty print

--- section4_weather-app/app.js
+++ section4_weather-app/app.js
@@ -8,5 +8,6 @@ request({ // this first argument of request is the options object
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=${secretKey}&location=1301%20lombard%20street%20philadelphia`, // including secret key in url by using Template string
     json: true
 }, (error, response, body) => { // This argument is the callback func that will return the response. Here (error, response, body) is the format specified by the package documentation for request package 
-    console.log(body);
+    // console.log(JSON.stringify(body)); // This will print the body but wrap the big objects inside body to [Object] when printing. Therefore we use pretty printing by using JSON.stringify
+    console.log(JSON.stringify(body, undefined, 2)); // 2nd arg is a set of properties that are not usually used so we leave them undefined. 3rd arg specifies the indentation spaces in the JSON printing
 });
