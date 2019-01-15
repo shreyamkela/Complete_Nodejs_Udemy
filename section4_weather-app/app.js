23b0f5b7c461db9c5d8f1f8d0a2f570bfcbea870
shrke <shreyamkela@gmail.com>
2019-01-14 23:54:11 -0800

    paths for address latitude and longitude

--- section4_weather-app/app.js
+++ section4_weather-app/app.js
@@ -8,6 +8,7 @@ request({ // this first argument of request is the options object
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=${secretKey}&location=1301%20lombard%20street%20philadelphia`, // including secret key in url by using Template string
     json: true
 }, (error, response, body) => { // This argument is the callback func that will return the response. Here (error, response, body) is the format specified by the package documentation for request package 
-    // console.log(JSON.stringify(body)); // This will print the body but wrap the big objects inside body to [Object] when printing. Therefore we use pretty printing by using JSON.stringify
+    // console.log(JSON.stringify(body)); // This will print the body but wrap the big objects inside body to [Object] when printing. Therefore we use pretty printing by using JSON.stringify, to format json objects when we print them to console
     console.log(JSON.stringify(body, undefined, 2)); // 2nd arg is a set of properties that are not usually used so we leave them undefined. 3rd arg specifies the indentation spaces in the JSON printing
+    console.log(`Address: ${body.results[0].providedLocation.location}\nLatitude: ${body.results[0].locations[0].displayLatLng.lat}\nLatitude: ${body.results[0].locations[0].displayLatLng.lng}`); // Use JSONview chrome extension to pretty print the json object when the url is passed in chrome. Hover cursor anywhere on the JSON output to see the full path for that particular key value pair, and rightclick for JSONview options to copy path
 });
