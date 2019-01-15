5dfec5f0c4703b05a2c016cafafb2984f2563beb
shrke <shreyamkela@gmail.com>
2019-01-15 00:56:58 -0800

    injecting address into url

--- section4_weather-app/app.js
+++ section4_weather-app/app.js
@@ -15,12 +15,18 @@ const argv = yargs
     .alias('help', 'h') //by alias() we can set the alias for help itself
     .argv; // .argv stores all the options into the var used for this yargs which in this case is 'argv'
 
-console.log(argv);
+//console.log(argv);
+
+// The adress input from command line has spaces which have to be converted to %20 (special character for space) before passing as the address identifier in the url
+// For these purposes we use encode and decode URI (Uniform resource identifier)
+
+var encodedAddress = encodeURIComponent(argv.a);
+console.log(encodedAddress);
 
 var secretKey = fs.readFileSync('secret-key.txt', 'utf-8'); // save the key from mapquestapi (i.e the map api) into a local file and read from into a variable, so that the key is not shared with others. secret-key.txt would only be present on your machine and will not be uploaded on git as we include secret-key.txt in gitignore. Not showing keys publicly is an essential practice for keys
 
 request({ // this first argument of request is the options object
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${secretKey}&location=1301%20lombard%20street%20philadelphia`, // including secret key in url by using Template string
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${secretKey}&location=${encodedAddress}`, // including secret key in url by using Template string
     json: true
 }, (error, response, body) => { // This argument is the callback func that will return the response. Here (error, response, body) is the format specified by the package documentation for request package 
     // console.log(JSON.stringify(body)); // This will print the body but wrap the big objects inside body to [Object] when printing. Therefore we use pretty printing by using JSON.stringify, to format json objects when we print them to console
