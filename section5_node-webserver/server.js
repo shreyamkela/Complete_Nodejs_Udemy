ccd0314d787f5f3b0536ffa1f75a322af9bf2ef2
shrke <shreyamkela@gmail.com>
2019-01-21 13:55:40 -0800

    intro to express middleware through static directory

--- section5_node-webserver/server.js
+++ section5_node-webserver/server.js
@@ -25,5 +25,14 @@ app.get('/bad', (req, res) => {
 });
 
 
+// We want to serve all contents in the public folder without having to set the path by different handlers in express, for each file in public folder. This can be done by using express middleware. WIth it we can add the path to a folder containing html files, css files, images, etc and that sets up the single handler that serves all the files in that folder
+// We provide the middleware function we want to use inside app.use(). We can also make our own middleware
+app.use(express.static(__dirname + '/public')); // By this we set a static directory. express.static() is the middleware function used to serve the contents of the public folder. Note that these contents would be available to the user at the root level i.e 'localhost:3000/help.html' and not 'localhost:3000/public/help.html'. What app.use(express.static(__dirname + '/public')) does is on each request, it looks in the public folder for a file that matches the name of the request path (i.e. help.html). If it finds it, then it serves that, if not then it sends the request to the route handlers. Therfore whatever the user enters in the url bar, express will first search in the public folder and if not found, it will go to check the individual handlers of path
+//__dirname is a predefined keyword that stores the root directory we are working with. Useful when you are moving your folders around
+
+
 // To let the application listen/start at a particular port number:
-app.listen(3000);
+// app.listen(3000);
+app.listen(3000, () => { // We can also pass a function as the second argument to listen
+    console.log('Server is up on port 3000');
+});
