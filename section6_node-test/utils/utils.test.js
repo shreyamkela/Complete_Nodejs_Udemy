d1ef6be34450b07bcde9b36f9879a1e2c698c234
shrke <shreyamkela@gmail.com>
2019-01-23 15:18:04 -0800

    using automatic nodemon script for testing

--- section6_node-test/utils/utils.test.js
+++ section6_node-test/utils/utils.test.js
@@ -3,6 +3,9 @@
 const utils = require('./utils'); // utils.js
 // In package.json, add under test key - "test": "mocha **/*.test.js" which says that testing done by mocha on every .test.js file in present in any/every folder of the repo
 // "npm test" inside this subtree will directly run mocha testing
+// use 'nodemon --exec "npm test"' to run testing with nodemon. --exec tell nodemon to execute a command instead of a file
+// In package.json scripts we can add this 'nodemon --exec "npm test"' as a script to us a shorted command like - test-watch. You would have to use "nodemon --exec \"npm test\"" for windows otherwise the internal double quotes would not let the command run. For other os you can use "nodemon --exec 'npm test'"
+// Use "npm run test-watch" to run the script
 it('should add two numbers', () => { // 'it' is a mocha function which defines what the output of the test cases should be. This can be read as 'It should add two numbers'. This is BDD or behaviour driven development. The first arg is a test string
     var res = utils.add(33,11);
    
