835288f5c1e381cc9ca2aa4d463afd0524b05e0d
shrke <shreyamkela@gmail.com>
2019-01-22 16:53:23 -0800

    removed the unrequired Info folder

--- section5_node-webserver/server.js
+++ section5_node-webserver/server.js
@@ -8,7 +8,7 @@ var app = express(); // make an app/application server that handles requests
 hbs.registerPartials(__dirname + '/views/partials'); // Registering an hbs partial is basically a code snippet that you can automatically copy paste wherever you want to use it by just typing the partial, without the need of typing out this snippet everywhere you want to use that snippet. Changes in hbs would not be monitered by nodemon therefore to apply changes in nodemon whenever you update hbs then use nodemon server.js -e js,hbs. -e tell nodemon to watch only/all js and hbs files for changes. -e is extension
 app.set('view engine', 'hbs'); // The 2 args inside set are a key value pair. Set various express related config. Here we are specifying that the view exngine for express that we are using is hbs. We can also set it as html for html view engine. The default directory that express uses to look for templates is 'views'. Therefore we save our templates in the views folder
 
-// app.use(express.static(__dirname + '/public')); // This is express middleware. The call to the public route should be after the maintainence.hbs as express executes in the order in which app.use is used in the code. If app.use(express.static(__dirname + '/public')); is above maintainance.hbs call, then public folder will still be served even if all the other handlers are not, when site is under maintainence.
+// app.use(express.static(__dirname + '/public')); // This is express middleware. The call to the public route should be after the maintainence.hbs as express executes middlewares in the order in which app.use is used in the code to define them. If app.use(express.static(__dirname + '/public')); is above maintainance.hbs call, then public folder will still be served even if all the other handlers are not, when site is under maintainence.
 // app.use() can be used to register your own middleware. Middleware can be setup in such a way that it checks the access privilege before it actually accesses that specific path/route. This is just one use case of custom express middleware
 
 app.use((req, res, next) => { //Registering your custom express middleware. next specifies what to do when this middleware call completes. Using this we can chain different middlewares. This is async operation so next has to be called otherwise the app handlers for requests will never fire and app pauses till next is called. This middleware moniters all of the requests to our server and therefore if next is not used, our server will not respond to any request
@@ -23,14 +23,17 @@ app.use((req, res, next) => { //Registering your custom express middleware. next
     next(); // If next is not used, server pauses here and no request is served
 });
 
-app.use((req, res, next) => { // Maintainance middleware
-    res.render('maintainance.hbs');
-    // next(); // As next is not called here, no handler executes after this so pinging any of the handlers will cal maintainance.hbs only. Thus with this simple call we can render all pages to a maintainance page, when the site is being updated
-    // Note - Call to the public route should be after the maintainence.hbs call as express executes in the order in which app.use is used in the code. If app.use(express.static(__dirname + '/public')); is above maintainance.hbs call, then public folder will still be served even if all the other handlers are not, when site is under maintainence. 
-    // Therefore include public call only after maintanance call
-});
+
+// MAINTAINANCE MIDDLEWARE - Can be commented out when the website is not under maintainance
+// app.use((req, res, next) => {  
+//     res.render('maintainance.hbs');
+//     // next(); // As next is not called here, no handler executes after this so pinging any of the handlers will cal maintainance.hbs only. Thus with this simple call we can render all pages to a maintainance page, when the site is being updated
+//     // Note - Call to the public route should be after the maintainence.hbs call as express executes middlewares in the order in which app.use is used in the code to define them. If app.use(express.static(__dirname + '/public')); is above maintainance.hbs call, then public folder will still be served even if all the other handlers are not, when site is under maintainence. 
+//     // Therefore include public call only after maintanance call
+// });
 
 app.use(express.static(__dirname + '/public')); // Public folder call. This is express middleware
+
 
 // registerPartials registers partial html/hbs snippets that can be injected into other html/hbs files. To inject dynamiclogic content into html/hbs we register helper functions for our use. Helpers can be used outside as well as inside of partials. As both partials and helpers use mustaches {{}}, for every {{somePartialOrHelper}} handlebars first checks whether somePartialOrHelper is a partial, if not then it checks whether it is a helper, if not then it checks if there is some data with the name somePartialOrHelper
 hbs.registerHelper('getCurrentYear', () => {
