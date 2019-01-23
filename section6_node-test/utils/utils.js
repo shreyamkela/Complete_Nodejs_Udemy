5e92ad935c6dfe21c7bd3faec22c83435b589c7f
shrke <shreyamkela@gmail.com>
2019-01-23 04:27:35 -0800

    tesing an add function with mocha

---
+++ section6_node-test/utils/utils.js
@@ -0,0 +1,2 @@
+module.exports.add = (a, b) => a + b; // Expressions syntax of arrow function
+// module.exports.add = (a, b) => a + b + 10; // to test for failing
---
+++ section6_node-test/utils/utils.test.js
@@ -0,0 +1,12 @@
+// <name>.test.js is the general naming convention for the file that saves the test cases of the <name>.js file
+
+const utils = require('./utils'); // utils.js
+// In package.json, add under test key - "test": "mocha **/*.test.js" which says that testing done by mocha on every .test.js file in present in any/every folder of the repo
+// "npm test" inside this subtree will directly run mocha testing
+it('should add two numbers', () => { // 'it' is a mocha function which defines what the output of the test cases should be. This can be read as 'It should add two numbers'. This is BDD or behaviour driven development. The first arg is a test string
+    var res = utils.add(33,11);
+
+    if (res !== 44) {
+        throw new Error(`Expected 44, but got ${res}`);
+    }
+}); 
